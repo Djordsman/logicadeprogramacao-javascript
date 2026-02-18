e6101daf69d924eedb0ada4061716bda1b923879
Djordsman <jorgecustodio_89@hotmail.com>
2026-02-18 20:17:55 -0300

    Conhecendo outros operadores lógicos e suas funções

--- operadores-logicos.js
+++ operadores-logicos.js
@@ -1,7 +1,17 @@
 // && (informativo ADICIONAL!!! "Maior EEE igual!!")
 
-const freteGratis = true;
-const estoque = true;
-console.log(freteGratis&&estoque);
+const freteGratis = false;
+const estoque = false;
 
-//Operador lógico 'OU': ||
+//console.log(freteGratis&&estoque);
+
+// Operador lógico 'OU' ||
+//console.log(freteGratis || estoque)
+
+// Operador lógico NOT (negação!) ela inverte a condição.
+//console.log(!freteGratis)
+
+// Dupla negação !! Usado apra converter um valor para seu equivalente booleano
+
+const nome = "Jords";
+console.log(!!nome)
